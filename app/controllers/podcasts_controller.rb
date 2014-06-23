d1b7290db88601340753f3a5f0e0fe4072639bc8
Eli Duke <elimduke@gmail.com>
2014-06-23 14:37:06 -0700

    import now pulls in multiple podcasts if necessary, removes unnecessary scaffold actions and views

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -14,81 +14,40 @@ class PodcastsController < ApplicationController
   def show
   end
 
-  # GET /podcasts/new
-  def new
-    @podcast = Podcast.new
-  end
-  
   def import
 
     doc = Nokogiri::HTML(open('http://thisamericanlife.org')).css('div.this-week').css('div.content')
 
-    this_week = doc.css("h3").text.split(":").first.strip.to_i
+    newest = doc.css("h3").text.split(":").first.strip.to_i
+    
+    last = Podcast.last.number
 
-    if this_week > Podcast.last.number
-
-      doc = Nokogiri::HTML(open("http://www.thisamericanlife.org/radio-archives/episode/#{this_week}")).css("div#content")
-
-      image = doc.css("div.image img").attribute('src')
-      number = doc.css("h1.node-title").text.split(":").first
-      title = doc.css("h1.node-title").text.split(":").last.strip
-      description = doc.css("div.description").text.strip
-      date = Date.parse(doc.css("div.date").text).strftime("%F")
-      mp3 = "http://podcast.thisamericanlife.org/podcast/#{this_week}.mp3"
+    if newest > last
       
-      Podcast.create!(number: number.to_i, title: title, description: description, date: date)
-
-      # puts "Image: " + image
-      # puts "Number: " + number
-      # puts "Title: " + title
-      # puts "Description: " + description
-      # puts "Date: " + date
-      # puts "MP3: " + mp3
+      episode = last + 1
       
+      while episode <= newest
+            
+        doc = Nokogiri::HTML(open("http://www.thisamericanlife.org/radio-archives/episode/#{episode}")).css("div#content")
+
+        number = doc.css("h1.node-title").text.split(":").first.to_i
+        title = doc.css("h1.node-title").text.split(":").last.strip
+        description = doc.css("div.description").text.strip
+        date = Date.parse(doc.css("div.date").text).strftime("%F")
+
+        Podcast.create!(number: number, title: title, description: description, date: date)
+
+        # image = doc.css("div.image img").attribute('src')
+        # mp3 = "http://audio.thisamericanlife.org/jomamashouse/ismymamashouse/#{episode}.mp3"
+      
+        episode += 1
+        
+      end
       redirect_to root_path
-
     else
-
-      puts "RE-RUN!"
-      
       redirect_to root_path
-
     end
     
-  end
-
-  # GET /podcasts/1/edit
-  def edit
-  end
-
-  # POST /podcasts
-  # POST /podcasts.json
-  def create
-    @podcast = Podcast.new(podcast_params)
-
-    respond_to do |format|
-      if @podcast.save
-        format.html { redirect_to @podcast, notice: 'Podcast was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @podcast }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @podcast.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /podcasts/1
-  # PATCH/PUT /podcasts/1.json
-  def update
-    respond_to do |format|
-      if @podcast.update(podcast_params)
-        format.html { redirect_to @podcast, notice: 'Podcast was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @podcast.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # DELETE /podcasts/1
