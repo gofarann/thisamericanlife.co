e432b5ac8b41cd2fddda73f51e058c8f2de4f20c
Eli Duke <elimduke@gmail.com>
2014-09-07 00:12:40 -0700

    removes unused podcast_uploader

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,16 +1,3 @@
-# CarrierWave.configure do |config|
-#   config.storage    = :aws
-#   config.aws_bucket = ENV['S3_BUCKET_NAME']
-#   config.aws_acl    = :public_read
-#   config.asset_host = 'http://talarchive.s3.amazonaws.com/'
-#   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
-# 
-#   config.aws_credentials = {
-#     access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-#     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-#   }
-# end
-
 AWS::S3::Base.establish_connection!(
   :access_key_id => ENV['AWS_ACCESS_KEY_ID'], 
   :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
