5a867dc00456dda9c59d8bc02a14215e18e1dc3c
Eli Duke <elimduke@gmail.com>
2015-03-31 21:35:38 -0700

    adds bootstrap style pagination with custom display for mobile

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,7 +1,7 @@
 Kaminari.configure do |config|
   config.default_per_page = 5
   # config.max_per_page = nil
-  config.window = 3
+  # config.window = 0
   # config.outer_window = 0
   # config.left = 0
   # config.right = 0
