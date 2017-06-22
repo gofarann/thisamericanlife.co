8f496f144e83830429be1e790aa06584628c15d7
Eli Duke <elimduke@gmail.com>
2017-06-22 01:03:04 -0700

    Removes explicit elasticsearch callbacks for episode.rb

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -3,18 +3,6 @@ class Episode < ActiveRecord::Base
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
 
-  after_commit on: [:create] do
-    __elasticsearch__.index_document
-  end
-
-  after_commit on: [:update] do
-    __elasticsearch__.update_document
-  end
-
-  after_commit on: [:destroy] do
-    __elasticsearch__.delete_document
-  end
-
   validates :number, :title, :description, :date, presence: true
   validates :number, uniqueness: true
 
